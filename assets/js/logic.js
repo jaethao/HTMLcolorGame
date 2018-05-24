8297bb3cb667e585319c3203b57943dc1534bd35
jaethao <mjathao@gmail.com>
2018-05-24 16:35:17 -0500

    working logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -105,13 +105,43 @@ $(document).ready(function(){
     "slateGray"
       ];
 
+//select all the square class
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[Math.floor(Math.random()*colors.length)];
+
+//pick four rand colors from the array
+//while loop to pick four random colors from the colors array
+var randColors = [];
+do {
+randColors[randColors.length] = colors.splice(Math.floor(Math.random() * colors.length) , 1)[0];
+} while (randColors.length < 4);
+
+//pick a random color from the randColors array
+var pickedColor = randColors[Math.floor(Math.random()*randColors.length)];
+
+//store the random color to the colorDisplay div
 var colorDisplay = document.getElementById("colorDisplay");
 
+//display the random color in the div
 colorDisplay.textContent = pickedColor;
 
+//loop thru the colors and squares
+for(var i = 0; i < squares.length; i++){
+//add four randColors to the squares
+squares[i].style.backgroundColor = randColors[i];
 
+//add click listeners to colorSquares
+squares[i].addEventListener("click", function(){
 
+  //grab color of click square and compare to pickedColor
+  var clickedColor = this.style.backgroundColor;
+
+  //compare color to pickedColor
+  if(clickedColor === pickedColor){
+    alert("Correct!");
+  }  else {
+    alert("Wrong!!!");
+  }
+});
+};
 
 });
